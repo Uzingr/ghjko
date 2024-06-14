2265edd73151e62b010bc9fd0f71eea2a0a5f012
Marcello Vangi <116904505+Uzingr@users.noreply.github.com>
2024-06-14 16:56:58 +0200

    Update setup.py

--- vicon_tracker_ros/setup.py
+++ vicon_tracker_ros/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'vicon_tracker_ros'
@@ -10,11 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/your_launch_file_name.py']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+        #(os.path.join('share', package_name), glob('resource/*rviz'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
+    maintainer='Your Name',
     maintainer_email='your_email@example.com',
     description='Your package description',
     license='Your license',
