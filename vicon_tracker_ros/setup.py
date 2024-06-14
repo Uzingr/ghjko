722b5c54f6e841c1d2d4a5d77a65f4777e5bf399
Marcello Vangi <116904505+Uzingr@users.noreply.github.com>
2024-06-14 16:42:18 +0200

    Update setup.py

--- vicon_tracker_ros/setup.py
+++ vicon_tracker_ros/setup.py
@@ -1,5 +1,27 @@
-entry_points={
-    'console_scripts': [
-        'pose_to_odometry = your_package_name.pose_to_odometry:main',
+from setuptools import setup
+
+package_name = 'vicon_tracker_ros'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/your_launch_file_name.py']),
     ],
-},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Your package description',
+    license='Your license',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pose_to_odometry = vicon_tracker_ros.pose_to_odometry:main',
+        ],
+    },
+)
