02fc4a0857c7f426fb01b5fd9717836c94d876b6
Marcello Vangi <116904505+Uzingr@users.noreply.github.com>
2024-07-02 18:41:14 +0200

    Update vicon_tracker_client.launch.py

--- vicon_tracker_ros/launch/vicon_tracker_client.launch.py
+++ vicon_tracker_ros/launch/vicon_tracker_client.launch.py
@@ -7,16 +7,25 @@ def generate_launch_description():
     ld = LaunchDescription()
     
     node_vicon_client = Node(
-        package = 'vicon_tracker_ros',
-        name = 'vicon_tracker_client',
-        executable = 'vicon_tracker_client',
-        parameters = [os.path.join(
+        package='vicon_tracker_ros',
+        name='vicon_tracker_client',
+        executable='vicon_tracker_client',
+        parameters=[os.path.join(
             get_package_share_directory('vicon_tracker_ros'),
             'config',
             'vicon_tracker_client_params.yaml'
         )]
     )
     
+    node_vicon_to_px4 = Node(
+        package='vicon_tracker_ros',
+        name='vicon_to_px4',
+        executable='vicon_to_px4',
+    )
+    
     ld.add_action(node_vicon_client)
+    ld.add_action(node_vicon_to_px4)
+    
     return ld
+
 
